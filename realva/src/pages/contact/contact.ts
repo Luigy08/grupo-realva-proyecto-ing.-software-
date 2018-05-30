4a03f3c72b541818a1ea143e522d3621dea60474
Milton Martínez <miltonmb@unitec.edu>
2018-05-30 13:03:19 -0600

    validaciones hechas en la parte del email

--- realva/src/pages/contact/contact.ts
+++ realva/src/pages/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, ActionSheetController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'page-contact',
@@ -8,14 +9,38 @@ import { Http } from '@angular/http';
 })
 export class ContactPage {
   Data: any = {};
+  contactForm: FormGroup;
+  username: AbstractControl;
+  email: AbstractControl;
+  message: AbstractControl;
 
-  constructor(public http: Http, nav: NavController, public actionSheetCtrl: ActionSheetController) {
+  constructor(public http: Http, nav: NavController, public actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController) {
     this.http = http;
+    this.contactForm = new FormGroup({
+      username: new FormControl(),
+      email: new FormControl(),
+      message: new FormControl
+    });
   }
   post() {
-    this.http.post("api/form/d34b479b-974b-53dd-9737-94d59ea03275/form-response", this.Data).subscribe();
-
+    if(this.contactForm.valid){
+      if(this.http.post("api/form/d34b479b-974b-53dd-9737-94d59ea03275/form-response", this.Data).subscribe() != null){
+        this.contactForm.reset();
+       this.presentAlert("Mensaje Enviado","Su mensaje ha sido enviado con éxito!","Ok");
+      }
+    }else{
+      this.presentAlert("Error","Ingrese todos sus datos de manera correcta!","Ok");
     }
-
   }
+
+  presentAlert(Title,SubTitle,Button) {
+    let alert = this.alertCtrl.create({
+      title: Title,
+      subTitle: SubTitle,
+      buttons: [Button]
+    });
+    alert.present();
+  }
+
+}
 
