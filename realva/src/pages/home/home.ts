7b19efb39197b955141194283b894393da1da7b9
MarioRaudales23 <marioraudales33@unitec.edu>
2018-06-04 16:37:49 -0600

    Product funciona

--- realva/src/pages/home/home.ts
+++ realva/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {LoginPage} from "../login/login";
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,7 +11,15 @@ import {LoginPage} from "../login/login";
 export class HomePage {
   login = LoginPage;
   constructor(public navCtrl: NavController) {
+    
 
+  }
+  @ViewChild(Slides) slides: Slides;
+
+  goToSlide() {
+    this.slides.slideTo(1, 15000);
+    this.slides.slideTo(2, 15000);
+    
   }
 
 }
