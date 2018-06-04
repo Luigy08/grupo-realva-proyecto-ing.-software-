5ac91f78a4930d23f1ce016c5b13ccf452c8c8ff
MarioRaudales23 <marioraudales33@unitec.edu>
2018-06-04 17:51:23 -0600

    login go back

--- realva/src/pages/login/login.ts
+++ realva/src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
 import { LoginRegisterProvider } from "../../providers/login-register/login-register";
+import {HomePage} from "../home/home";
 
 /**
  * Generated class for the LoginPage page.
@@ -20,6 +21,7 @@ export class LoginPage {
   loginForm: FormGroup;
   username: AbstractControl;
   email: AbstractControl;
+  page:HomePage;
   constructor(public navCtrl: NavController, public navParams: NavParams, public loginRegister: LoginRegisterProvider) {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
@@ -28,6 +30,9 @@ export class LoginPage {
   }
   clear(){
     this.loginForm.reset();
+  }
+  move(){
+    this.navCtrl.pop();
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
