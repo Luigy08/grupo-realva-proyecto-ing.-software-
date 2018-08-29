39719859c5bc123bbeb81f2b8b537159ced74327
jorgemorazan <jorgemorazan@unitec.edu>
2018-08-29 17:18:23 -0600

    almost done
    tabs are almost done

--- realva/src/pages/about/about.ts
+++ realva/src/pages/about/about.ts
@@ -5,6 +5,7 @@ import {LoginPage} from "../login/login";
 import {RegisterPage} from "../register/register";
 import {AdminPage} from "../admin/admin";
 import {ProfilePage} from "../profile/profile"
+import { DECELERATION_FRICTION } from 'ionic-angular/umd/components/picker/picker-options';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -14,8 +15,54 @@ export class AboutPage {
   register = RegisterPage;
   Admin = AdminPage;
   Profile = ProfilePage;
+  iClicked:boolean = false;
   constructor(public navCtrl: NavController,public LoginRegister: LoginRegisterProvider) {
-
+    
   }
+  
+  openCity(cityName, num) {
+    // Declare all variables
 
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    if(num == 1){
+      this.iClicked = true;
+      console.log(this.iClicked);
+    }
+    if(this.iClicked){
+      if(cityName == "London"){
+        document.getElementById("default").className = "tablinks active";
+      } 
+      
+      if(cityName == "Paris"){
+        document.getElementById("vision").className = "tablinks active";
+      }
+      if(cityName == "Tokyo"){
+        document.getElementById("valores").className = "tablinks active";
+      }
+    } else {
+      if(cityName == "London"){
+        document.getElementById("default").className = "tablinks active";
+      } 
+    }
+    
+    //evt.currentTarget.className += "active";
+  }
+  getiClicked(){
+    return this.iClicked;
+  }
 }
