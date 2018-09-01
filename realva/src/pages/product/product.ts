18c503480359ad118e25bf57e0e3189e20df8a9a
jorgemorazan <jorgemorazan@unitec.edu>
2018-08-31 22:13:02 -0600

    still trying
    title

--- realva/src/pages/product/product.ts
+++ realva/src/pages/product/product.ts
@@ -51,4 +51,18 @@ export class ProductPage {
   push(p: any) {
     this.navCtrl.push(ShowproductPage, {productoEntrada: p});
   }
+
+  highlightMe(information){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  }
 }
